560fd6cf62658a45fcb8b48428ad67a9ad437dae
Seungjin Lee <dltmdwls154@gmail.com>
2025-07-15 22:21:45 +0900

    Test

--- app/root.tsx
+++ app/root.tsx
@@ -11,18 +11,6 @@ import {
 import "./app.css";
 import type { ReactNode } from "react";
 
-export const links: LinksFunction = () => [
-  { rel: "preconnect", href: "https://fonts.googleapis.com" },
-  {
-    rel: "preconnect",
-    href: "https://fonts.gstatic.com",
-    crossOrigin: "anonymous",
-  },
-  {
-    rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
-  },
-];
 
 export function Layout({ children }: { children: ReactNode }) {
   return (
