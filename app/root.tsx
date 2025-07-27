b5d8922f5ac4e5603bb86916a1523faa616bd1d9
Seungjin Lee <dltmdwls154@gmail.com>
2025-07-27 20:49:28 +0900

    Test

--- app/root.tsx
+++ app/root.tsx
@@ -17,7 +17,7 @@ export function Layout({ children }: { children: ReactNode }) {
     <html lang="ko">
       <head>
         <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1 " />
+        <meta name="viewport" content="width=device-width, initial-scale=1 viewport-fit=cover user-scalable=no" />
         <Meta />
         <Links />
       </head>
